e152c956591b73eb76ed65bcf8bc37bed259cd3d
Tennyleaz <tennyleaz@gmail.com>
2018-07-12 13:31:16 +0800

    加入專案檔案。

---
+++ Mail to uri/MailToUtility.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Mail_to_uri
+{
+    public class MailToUtility
+    {
+        public static string SendMailToURI(List<string> vAddress, List<string> vCC, List<string> vBCC, string strSubject, string strBody)
+        {
+            string strURL = @"mailto:";
+
+            // address
+            if (vAddress != null && vAddress.Count > 0)
+            {
+                string strAddress = string.Join(";", vAddress);
+                if (!string.IsNullOrEmpty(strAddress))
+                    strURL += strAddress;
+            }
+            strURL += @"?";
+
+            // subject
+            if (!string.IsNullOrEmpty(strSubject))
+                strURL += @"subject=" + System.Web.HttpUtility.UrlEncode(strSubject);
+
+            // body
+            if (!string.IsNullOrEmpty(strBody))
+                strURL += @"&body=" + System.Web.HttpUtility.UrlEncode(strBody);
+
+            // CC
+            if (vCC != null && vCC.Count > 0)
+            {
+                string strCC = string.Join(";", vCC);
+                if (!string.IsNullOrEmpty(strCC))
+                    strURL += @"&cc=" + strCC;
+            }
+
+            // BCC        
+            if (vBCC != null && vBCC.Count > 0)
+            {
+                string strBCC = string.Join(";", vBCC);
+                if (!string.IsNullOrEmpty(strBCC))
+                    strURL += @"&bcc=" + strBCC;
+            }
+
+            try
+            {
+                System.Diagnostics.Process.Start(strURL);
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Test if HKEY_CLASSES_ROOT\mailto\shell\ has value not set to (value not set)
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsMailClientInstalled()
+        {
+            // HKEY_CLASSES_ROOT\mailto\shell\
+            bool bReturn = false;
+            string subKeyPath = @"mailto\shell\";
+            try
+            {
+                RegistryKey key = Registry.ClassesRoot.OpenSubKey(subKeyPath);
+                if (key != null)
+                {
+                    object objValue = key.GetValue("");
+                    if (objValue != null)
+                    {
+                        if (key.GetValueKind("") != RegistryValueKind.String)
+                        {
+                            bReturn = false;
+                            MessageBox.Show("Default mailto value is not a string type");
+                        }
+                        else
+                        {
+                            string strValue = Convert.ToString(objValue);
+                            if (strValue == @"(value not set)")
+                            {
+                                bReturn = false;
+                            }
+                            else
+                            {
+                                bReturn = true;                                
+                            }
+                            MessageBox.Show("Default mailto value is " + strValue);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Default value does not exist");
+                        bReturn = false;
+                    }
+                    key.Close();
+                    key.Dispose();
+                    key = null;
+                }
+                else
+                    MessageBox.Show("HKEY_CLASSES_ROOT\\mailto\\shell\\\ndoes not exist");
+            }
+            catch { }
+            return bReturn;
+        }
+
+        /// <summary>
+        /// Test if HKEY_CLASSES_ROOT\mailto\shell\open\command\ has value not set to C:\windows\system32\
+        /// </summary>
+        /// <returns></returns>
+        public static bool TestOpenCommand()
+        {
+            bool bReturn = false;
+            string subKeyPath = @"mailto\shell\open\command\";
+            try
+            {
+                RegistryKey key = Registry.ClassesRoot.OpenSubKey(subKeyPath);
+                if (key != null)
+                {
+                    object objValue = key.GetValue("");
+                    if (objValue != null)
+                    {
+                        if (key.GetValueKind("") != RegistryValueKind.String)
+                        {
+                            bReturn = false;
+                            MessageBox.Show("Default mailto command value is not a string type");
+                        }
+                        else
+                        {
+                            string strValue = Convert.ToString(objValue);
+                            if (strValue != null)
+                            {
+                                if (strValue.IndexOf(@"C:\windows\system", StringComparison.OrdinalIgnoreCase) >= 0)
+                                    bReturn = false;
+                                else
+                                    bReturn = true;
+                            }
+                            MessageBox.Show("Default mailto command value is " + strValue);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Default command value does not exist");
+                        bReturn = false;
+                    }
+                    key.Close();
+                    key.Dispose();
+                    key = null;
+                }
+                else
+                    MessageBox.Show("HKEY_CLASSES_ROOT\\mailto\\shell\\open\\command\\\ndoes not exist");
+            }
+            catch { }
+            return bReturn;
+        }
+    }
+}
---
+++ Mail to uri/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mail_to_uri
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbContent.Text += "\n第二行...";
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            /// mailto:manish@simplygraphix.com?
+            /// subject=Feedback for webdevelopersnotes.com
+            /// &body=The Tips and Tricks section is great 
+            /// &cc=anotheremailaddress@anotherdomain.com
+            /// &bcc = onemore@anotherdomain.com
+            ///           
+
+            string strAddress = tbEmail.Text;
+            string strSubject = tbSubject.Text;
+            string strBody = tbContent.Text;
+            string strCC = tbCC.Text;
+            string strBCC = tbBCC.Text;
+
+            char[] separators = { ';' };
+            List<string> vAddress= new List<string>();            
+            vAddress.AddRange(strAddress.Split(separators, StringSplitOptions.RemoveEmptyEntries));
+
+            List<string> vCC = new List<string>();
+            vCC.AddRange(strCC.Split(separators, StringSplitOptions.RemoveEmptyEntries));
+
+            List<string> vBCC = new List<string>();
+            vBCC.AddRange(strBCC.Split(separators, StringSplitOptions.RemoveEmptyEntries));
+
+            string strResult = MailToUtility.SendMailToURI(vAddress, vCC, vBCC, strSubject, strBody);
+            if (!string.IsNullOrEmpty(strResult))
+                MessageBox.Show(strResult);
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            /// Windows 7：HKEY_CLASSES_ROOT\mailto\ 可能不存在，IE需要設定Google工具列或是類似的工具，才能開啟mailto:連結
+            /// Windows 8：HKEY_CLASSES_ROOT\mailto\ 會存在，但是下面不一定有東西
+            /// Windows 10：HKEY_CLASSES_ROOT\mailto\shell\open\command\ 會存在，預設值可能是 C:\windows\system32\rundll32.exe
+
+            bool b = MailToUtility.IsMailClientInstalled();
+            if (!b)
+            {
+                b = MailToUtility.TestOpenCommand();
+                if (!b)
+                    MessageBox.Show("no email client detected");
+                else
+                    MessageBox.Show("email client detected!");
+            }
+            else
+                MessageBox.Show("email client detected!");
+        }
+    }
+}
